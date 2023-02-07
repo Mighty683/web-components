82c9b1d3693a24c4e18f4ea65c85df832800756d
Mighty683 <tomasz93szarek@gmail.com>
2023-02-08 00:14:59 +0100

    chore: cleaup after input

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
-import { resolve } from "path";
+import { resolve } from 'path';
 
 /** @type {import('vite').UserConfig} */
 
 export default {
-  appType: "custom",
   build: {
     lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "web-components",
-      fileName: "index",
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'web-components',
+      fileName: 'index',
     },
   },
 };
